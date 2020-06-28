d3fba18a34d2571d35283d1418f5cd2b10bc1bde
eason <breaking0203@gmail.com>
2020-06-28 23:51:25 +0800

    init main.cpp macro

--- main.cpp
+++ main.cpp
@@ -18,13 +18,13 @@
 #include "Snake.h"
 
 //whether cout or not(print map)
-#define COUT 0
+#define COUT 1
 //whether cout result or not
 #define COUT_RESULT 1
 //write result to file or not
 #define WRITE_RESULT_TO_FILE 0
 //skip map index to 
-#define SKIP_POINT 201
+#define SKIP_POINT 0
 //milli second per frame
 #define milli_sec_per_frame 60
 
